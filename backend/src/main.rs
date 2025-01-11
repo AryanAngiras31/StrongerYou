86e891bde34a765252ca4f120228b19768c61e26
Aryan <aryan.s.angiras@gmail.com>
2025-01-11 10:06:20 +0000

    starting to code backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    HttpServer::new(|| App::new().service(create_routine))
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
 }
+#[derive(Deserialize, Serialize)]
+pub struct Routine {
+    name: String,
+    exercise_list: Vec<Exercise>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Exercise {
+    name: String,
+    exercise_type: ExerciseType,
+}
+
+pub enum ExerciseType {
+    Single,
+    Double,
+}
+
+#[actix_web::post("/routines/create/")]
+async fn create_routine(routine_details: web::Path<(Routine)>) -> impl Responder {}
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello Worl!</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
