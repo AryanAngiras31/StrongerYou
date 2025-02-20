bc10a519bcd00c155bdcc43ce8ecc211ffca616b
Aryan Angiras <aryan.s.angiras@gmail.com>
2025-02-20 14:46:47 +0530

    Backend : Implementing markers endpoint v3.4 - Fixed 404 - not found error

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -246,10 +246,14 @@ async fn main() -> std::io::Result<()> {
             .service(routines::modify_routine)
             .service(routines::delete_routine)
             .service(routines::test_route)
-            .service(web::scope("").configure(exercises::init_routes))
-            .service(web::scope("").configure(markers::init_routes))
+            .configure(configure_routes)
     })
     .bind(("127.0.0.1", port))?
     .run()
     .await
+}
+
+fn configure_routes(cfg: &mut web::ServiceConfig) {
+    exercises::init_routes(cfg);
+    markers::init_routes(cfg);
 }
