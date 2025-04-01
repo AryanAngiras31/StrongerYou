5aceea6643c84b6504cd5e3f4d1cd500882b1f8c
Aryan Angiras <aryan.s.angiras@gmail.com>
2025-04-01 09:20:55 +0530

    Creating services\api.ts file - Created get_routine_by_name - fixed url by switching to backticks to make it an interpolaetd string

--- frontend/StrongerYou/src/services/api.ts
+++ frontend/StrongerYou/src/services/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const BASE_URL = 'https://localhost:8080/';
 
-// Implementing APIs for routines.rs
+// Routines API
 
 // 1. Fetch RoutineID by RoutineName
 export const get_routine_by_name = async (name : string) => {
   try {
-    const response = await axios.get('${BASE_URL}/routines/name',
+    const response = await axios.get(`${BASE_URL}/routines/name`,
         {
             params: { name },
             headers: {
