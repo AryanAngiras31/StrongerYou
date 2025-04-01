f83bffd656540365c30eff31411e51de79eea8dd
Aryan Angiras <aryan.s.angiras@gmail.com>
2025-04-01 09:15:30 +0530

    Creating services\api.ts file - Created get_routine_by_name

--- frontend/StrongerYou/src/services/api.ts
+++ frontend/StrongerYou/src/services/api.ts
@@ -2,5 +2,24 @@ import axios from 'axios';
 
 const BASE_URL = 'https://localhost:8080/';
 
-// Implementing APIs for exercises.rs
+// Implementing APIs for routines.rs
 
+// 1. Fetch RoutineID by RoutineName
+export const get_routine_by_name = async (name : string) => {
+  try {
+    const response = await axios.get('${BASE_URL}/routines/name',
+        {
+            params: { name },
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }
+    );
+    return response.data;
+  }
+  catch (error) {
+    console.error('Error fetching routine by name:', error);
+    throw error;
+  }
+};
